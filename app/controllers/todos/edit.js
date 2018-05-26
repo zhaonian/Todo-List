aad5832526ddad9f0afbd8be93701c9d924ad2e2
Zhaonian Luan <zhaonial@uci.edu>
2018-05-25 22:25:38 -0700

    added update single feature

--- app/controllers/todos/edit.js
+++ app/controllers/todos/edit.js
@@ -1,3 +1,21 @@
 import Controller from '@ember/controller';
 
-export default Controller.extend({});
+export default Controller.extend({
+        actions: {
+                editTodo: function(id) {
+                        let self = this;
+                        let title = this.get('model.title');
+                        let date = this.get('model.date');
+                        let body = this.get('model.body');
+
+                        this.store.findRecord('todo', id).then(((todo) => {
+                                todo.set('title', title);
+                                todo.set('date', new Date(date));
+                                todo.set('body', body);
+
+                                todo.save();
+                                self.transitionTo('todos');
+                        }));
+                }
+        }
+});
