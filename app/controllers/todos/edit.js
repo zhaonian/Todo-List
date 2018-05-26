28f12518527a02949b1b6e83b73f533400592ab4
Zhaonian Luan <zhaonial@uci.edu>
2018-05-25 22:30:25 -0700

    added delete single todo feature

--- app/controllers/todos/edit.js
+++ app/controllers/todos/edit.js
@@ -2,20 +2,31 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
         actions: {
-                editTodo: function(id) {
+                editTodo: id => {
                         let self = this;
                         let title = this.get('model.title');
                         let date = this.get('model.date');
                         let body = this.get('model.body');
 
-                        this.store.findRecord('todo', id).then(((todo) => {
+                        this.store.findRecord('todo', id).then(todo => {
                                 todo.set('title', title);
                                 todo.set('date', new Date(date));
                                 todo.set('body', body);
 
                                 todo.save();
                                 self.transitionTo('todos');
-                        }));
+                        });
+                },
+
+                deleteTodo: id => {
+                        let self = this;
+
+                        this.store.findRecord('todo', id).then(todo => {
+                                todo.deleteRecord();
+
+                                todo.save();
+                                self.transitionTo('todos');
+                        });
                 }
         }
 });
